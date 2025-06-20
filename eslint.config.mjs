591a5d8babb151da0a3bb05414da84c8b9ff6229
SharpDevOps10 <td090833@gmail.com>
2025-06-20 12:07:15 +0300

    Add a basic project structure

---
+++ eslint.config.mjs
@@ -0,0 +1,55 @@
+import js from '@eslint/js';
+import parser from '@typescript-eslint/parser';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import prettierPlugin from 'eslint-plugin-prettier';
+
+/** @type {import("eslint").Linter.FlatConfig[]} */
+export default [
+  js.configs.recommended,
+
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser,
+      parserOptions: {
+        project: './tsconfig.json',
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+      prettier: prettierPlugin,
+    },
+    ignores: ['dist/**', 'node_modules/**'],
+    rules: {
+      '@typescript-eslint/interface-name-prefix': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        { argsIgnorePattern: '^type$' },
+      ],
+      semi: ['error', 'always'],
+      'comma-dangle': ['error', 'always-multiline'],
+      'arrow-parens': ['error', 'always'],
+      'arrow-spacing': 'error',
+      'block-spacing': 'error',
+      'brace-style': 'error',
+      'comma-spacing': 'error',
+      'func-call-spacing': 'error',
+      'rest-spread-spacing': 'error',
+      'object-curly-spacing': ['error', 'always'],
+      indent: ['error', 2],
+      'key-spacing': 'error',
+      'keyword-spacing': 'error',
+      'space-before-function-paren': 'error',
+      'new-parens': 'error',
+      quotes: ['error', 'single'],
+      'space-before-blocks': 'error',
+      'space-in-parens': 'error',
+      eqeqeq: ['error', 'smart'],
+      'eol-last': ['error', 'always'],
+    },
+  },
+];
